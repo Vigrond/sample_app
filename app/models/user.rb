5034223ab0a97a5f5286d577bd3bdc6ad9d974fa
Vigrond <devitt.cs@gmail.com>
2014-04-09 16:16:20 -0700

    chap10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+  
   before_save { email.downcase! }
   before_create :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
@@ -10,7 +12,6 @@ class User < ActiveRecord::Base
 
   has_secure_password
    
-   
   def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
@@ -18,6 +19,11 @@ class User < ActiveRecord::Base
   def User.hash(token)
     Digest::SHA1.hexdigest(token.to_s)
   end
+  
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
 
   private
 
